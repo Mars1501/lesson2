14e25e8457578990cd66d2ccc355d5063a228418
Denis <den.batuev99@gmail.com>
2022-06-30 22:15:03 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10
+
+int number = new Random().Next(100, 999);
+Console.WriteLine("Исходное число " + number);
+int number2 = (number / 10) % 10;
+Console.WriteLine("Втрое число " + number2);
