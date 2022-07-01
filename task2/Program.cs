0b3841ed7b22f106ccd624a69aeefc8e02dd91c8
Denis <den.batuev99@gmail.com>
2022-07-01 19:58:28 +0300

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13
+
+
+
+void number(int x)
+{
+    int div = 1;
+    while(x / div >= 10)
+    {
+        div *= 10;
+    
+}
+if(div < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int digit = (x / (div / 100)) % 10;
+    Console.WriteLine("Третье число {0}", digit);
+}
+}
+
+
+number(645);
+number(78);
+number(32679);
+
---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] day = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+
+Console.Write("Введите целое число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    Console.Write("Это " + day[number - 1]);
+    if (number == 6 || number == 7)
+    {
+        Console.Write(" - выходной день.");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня не существует!");
+}
